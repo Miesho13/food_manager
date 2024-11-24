e7dce6a14c316730c3ff4730a35cc882a6267703
Marcin <marcin.ryzewskii@gmail.com>
2024-11-24 22:43:49 +0100

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module food_manager
+
+go 1.23.3
---
+++ web/data/food.js
@@ -0,0 +1,46 @@
+{
+   "foods": [
+       {
+            "name": "Makaron ze szpinakiem",
+            "type": "dish",
+            "kcal": 486,
+            "carbon": 61,
+            "proteins": 14,
+            "fat": 19,
+            "product": {
+                "Szpinak": 1,
+                "Makaron Tiagatele": 2,
+                "Śmietana 30%": 2,
+                "Suszone pomidory": 2
+            }
+       },
+       {
+            "name": "Baigiel sadzony",
+            "type": "dish",
+            "kcal": 494,
+            "carbon": 59,
+            "proteins": 17,
+            "fat": 21,
+            "product": {
+                "Rukola": "15g",
+                "Majonez": "10g",
+                "Masło": "5g",
+                "Bułka ziemniaczana": 1,
+                "Jajo ": 1,
+                "Awokado": "35g"
+            } 
+       },
+       {
+            "name": "Tibia",
+            "type": "dish",
+            "kcal": 52,
+            "carbon": 28,
+            "proteins": 14,
+            "fat": 14,
+            "product": {
+                "Peczatka": 1,
+                "Karniszon": 2
+            } 
+       }
+   ]
+}
