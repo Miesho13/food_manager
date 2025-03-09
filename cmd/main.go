47bd870b506aa6be0767acdc2a3aecd6fba9ece4
Miesho <marcin.ryzewskii@gmail.com>
2025-03-09 16:28:17 +0100

    Add log handle for the server.

--- cmd/main.go
+++ cmd/main.go
@@ -3,14 +3,22 @@ package main
 import (
     "fmt"
     "net/http"
+    "time"
 )
 
+func loggingMiddleware(next http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        fmt.Printf("[%s] %s %s from %s\n", time.Now().Format(time.RFC3339), r.Method, r.URL.Path, r.RemoteAddr)
+        next.ServeHTTP(w, r)
+    })
+}
+
 func main() {
+    fs := http.FileServer(http.Dir("./web"))
+    http.Handle("/", loggingMiddleware(fs))
 
-	http.Handle("/", http.FileServer(http.Dir("./web")))
-
-    println("Server stat: 127.0.0.1:6565")
+    fmt.Println("Server started at: http://127.0.0.1:6565")
     if err := http.ListenAndServe("0.0.0.0:6565", nil); err != nil {
-        fmt.Println("[ERROR] Server can't be started")
+        fmt.Println("[ERROR] Server can't be started:", err)
     }
 }
